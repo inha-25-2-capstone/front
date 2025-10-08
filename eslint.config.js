5c6df687dbca68b8387d1ebbbac2574654dc0061
GaHee Im <gaaahmon@gmail.com>
2025-10-08 18:43:12 +0900

    fix: eslint.config.js 오류 1

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import importPlugin from 'eslint-plugin-import'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
@@ -10,22 +9,22 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  
   {
     files: ['**/*.{ts,tsx}'],
     plugins: { react, 'react-hooks': reactHooks, import: importPlugin, 'simple-import-sort': simpleImportSort },
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      react.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: { ecmaVersion: 2020, globals: globals.browser },
     rules: {
       'react/react-in-jsx-scope': 'off',
       'import/order': 'off',
       'simple-import-sort/imports': 'warn',
       'simple-import-sort/exports': 'warn',
+      // react-hooks recommended
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
     settings: {
       react: { version: 'detect' },
